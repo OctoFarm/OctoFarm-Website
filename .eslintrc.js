2a132afbf830a15b5e69aba7f087cc782413b553
James <james@vexica.com>
2021-08-05 08:00:46 +0100

    remove server from website

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
+    browser: true,
     node: true,
-    jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  extends: [
+    "plugin:vue/essential",
+    "plugin:vue/strongly-recommended",
+    "@vue/airbnb",
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: "babel-eslint",
+    sourceType: "module",
+  },
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "linebreak-style": 0,
+    "max-len": [
+      "error",
+      {
+        code: 300,
+        ignoreComments: true,
+        ignoreUrls: true,
+      },
+    ],
+    "global-require": "off",
+    quotes: ["error", "double"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
