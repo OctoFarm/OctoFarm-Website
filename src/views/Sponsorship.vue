315e8eda61a41ccb48a58df001b6b86a07db6c09
NotExpectedYet <online@notexpectedyet.com>
2021-08-12 06:25:53 +0100

    add buy me a coffee

--- src/views/Sponsorship.vue
+++ src/views/Sponsorship.vue
@@ -15,19 +15,37 @@
             Become a Sponsor / Donate
           </v-card-title>
 
-          <v-card-text>
+          <v-card-text class="px-15">
             This project is built by an English bloke in his spare time, around a 9-5 job, to boot.
             If OctoFarm has helped you out at all then I'd be grateful if you could donate towards
             the project. It keeps me going and heck, who doesn't dream of been able to work on their
             passion project daily.
           </v-card-text>
+          <v-row>
+            <v-col>
+              <v-btn
+                v-for="spon in methods_of_sponsor"
+                :key="spon.title"
+                class="ma-5"
+                outlined
+                rounded
+                text
+                :href="spon.url"
+                target="_blank"
+                :color="spon.btn_colour"
+              >
+                <v-icon>{{ spon.icon }}</v-icon>  {{ spon.title }}
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
     <v-row>
       <v-col
         sm="12"
-        lg="6"
+        md="6"
+        lg="8"
       >
         <v-card>
           <v-row
@@ -58,7 +76,7 @@
               v-for="spon in methods_of_sponsor"
               :key="spon.title"
               sm="12"
-              md="6"
+              lg="6"
             >
               <v-card
                 class="mx-auto"
@@ -99,7 +117,8 @@
       </v-col>
       <v-col
         sm="12"
-        lg="6"
+        md="6"
+        lg="4"
       >
         <v-card>
           <v-row
@@ -130,23 +149,23 @@
             <v-col
               v-for="spon in $store.state.statistics.patreons.websitePledges"
               :key="spon.name"
-              xl="3"
+              sm="12"
+              md="12"
               lg="6"
-              md="6"
             >
               <v-card
                 outlined
                 class="ma-2 text-center"
               >
                 <v-list-item two-line>
+                  <v-avatar
+                    class="profile"
+                    color="grey"
+                    size="50"
+                  >
+                    <v-img :src="spon.thumb" />
+                  </v-avatar>
                   <v-list-item-content>
-                    <v-avatar
-                      class="profile"
-                      color="grey"
-                      size="100"
-                    >
-                      <v-img :src="spon.thumb" />
-                    </v-avatar>
                     <v-list-item-title class="headline mb-1">
                       {{ spon.name }}
                     </v-list-item-title>
@@ -165,43 +184,6 @@
 export default {
   data: () => ({
     tab: null,
-    sponsors: [
-      {
-        name: "Derek",
-        url_title: "3D Printed Debris",
-        url: "https://www.3dprinteddebris.com/",
-      },
-      {
-        name: "Insertion",
-        url_title: "",
-        url: "",
-      },
-      {
-        name: "lactose",
-        url_title: "",
-        url: "",
-      },
-      {
-        name: "Paul Helinski",
-        url_title: "",
-        url: "",
-      },
-      {
-        name: "Josh Wright",
-        url_title: "",
-        url: "",
-      },
-      {
-        name: "3DCT",
-        url_title: "",
-        url: "",
-      },
-      {
-        name: "Feldherr",
-        url_title: "",
-        url: "",
-      },
-    ],
     methods_of_sponsor: [
       {
         title: "Paypal",
@@ -211,6 +193,15 @@ export default {
         icon: "mdi-credit-card",
         btn_text: "Dontate",
         url: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QGBJML3E7268L&source=url",
+      },
+      {
+        title: "Buy me a Steak!",
+        text: "Buy me a steak (coffee) is dual purpose, you can make longer term donations and also short one time donations if you don't have paypal / patreon. There are some kick backs offered too!",
+        text2: "Lower level funding from this source will go towards James himself for steak! Higher levels are directly put back into the project as stated when signing up.",
+        btn_colour: "orange",
+        icon: "mdi-coffee",
+        btn_text: "Buy me a coffee",
+        url: "https://buymeacoffee.com/OctoFarm",
       },
       {
         title: "Patreon",
@@ -225,7 +216,7 @@ export default {
       {
         title: "Full Time Sponsor",
         text: "As mentioned above I code this application around my full time job. If any persons or company would like to sponsor me to work on this full time then please contact me using the button below.\n",
-        text2: "I have massive plans for the development future of this application and having a full time sponsor would allow me to push updates much more frequently and continue supporting the wonderful OctoPrint and now OctoFarm communities.",
+        text2: "I have massive plans for the development future of this application and having a full time sponsor would allow me to push updates much more frequently and continue supporting the wonderful OctoPrint and now OctoFarm communities. ",
         btn_colour: "secondary",
         icon: "mdi-heart",
         btn_text: "Contact",
